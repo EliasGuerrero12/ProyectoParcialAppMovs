ae9760e5c3614ffadae0f40259cad150f831bd40
EliasGuerrero12 <jesusguerreropw12@gmail.com>
2026-02-15 22:42:11 -0600

    CambiosDiscografia

--- AppMovilesParcial1/DiscografiaPage.xaml.cs
+++ AppMovilesParcial1/DiscografiaPage.xaml.cs
@@ -5,15 +5,10 @@ public partial class DiscografiaPage : ContentPage
     public DiscografiaPage()
     {
         InitializeComponent();
+    }
 
-        AlbumList.ItemsSource = new List<string>
-        {
-            "La vida era mas corta (2025)",
-            "Bajo de la piel (2025)",
-            "166 (2024)",
-            "111 (2023)",
-            "En Dormir Sin Madrid (2023)",
-            "511 (2023)"
-        };
+    private void OnAlbumLaVidaClicked(object sender, EventArgs e)
+    {
+        CancionesLaVida.IsVisible = !CancionesLaVida.IsVisible;
     }
 }
