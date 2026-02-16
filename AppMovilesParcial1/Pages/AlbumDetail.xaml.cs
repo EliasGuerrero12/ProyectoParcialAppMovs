a5837a4de3f21ce252d310128f7a8758a6ef5620
Debainilla <debanhiabigailla@gmail.com>
2026-02-16 11:33:46 -0600

    Update AlbumDetail
    Updating Album Detail XAML and CS with correct names of Album

--- AppMovilesParcial1/Pages/AlbumDetail.xaml.cs
+++ AppMovilesParcial1/Pages/AlbumDetail.xaml.cs
@@ -1,12 +1,18 @@
 using AppMovilesParcial1.Models;
+
 namespace AppMovilesParcial1.Pages;
 
 public partial class AlbumDetail : ContentPage
 {
-	public AlbumDetail()
-	{
-		InitializeComponent();
+    public AlbumDetail(Album album)
+    {
+        InitializeComponent();
 
-		
+        // Llenar controles con el modelo
+        AlbumImage.Source = album.Image;
+        AlbumTitle.Text = album.Name;
+        AlbumArtist.Text = "Álbum";
+        AlbumReleaseDate.Text = "Año: " + album.Year;
+        AlbumDescription.Text = album.Description;
     }
 }
