6b5a3f4e80475efd92637d687bf185c01e5daf26
EliasGuerrero12 <jesusguerreropw12@gmail.com>
2026-02-16 15:43:18 -0600

    CambioFlyout

--- AppMovilesParcial1/DiscografiaPage.xaml.cs
+++ AppMovilesParcial1/DiscografiaPage.xaml.cs
@@ -5,10 +5,16 @@ public partial class DiscografiaPage : ContentPage
     public DiscografiaPage()
     {
         InitializeComponent();
-    }
 
-    private void OnAlbumLaVidaClicked(object sender, EventArgs e)
-    {
-        CancionesLaVida.IsVisible = !CancionesLaVida.IsVisible;
+        List<string> albumes = new List<string>
+        {
+            "La vida era mas corta (2025)",
+            "166 (2024)",
+            "111 (2023)",
+            "En dormir sin Madrid (2023)",
+            "511 (2023)"
+        };
+
+        ListaAlbumes.ItemsSource = albumes;
     }
 }
