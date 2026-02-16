9ef9c04685d4dc67567b699db1d05e6a099d6ae2
Debainilla <debanhiabigailla@gmail.com>
2026-02-16 13:40:25 -0600

    Add Images
    Adding images to albums of Milo

--- AppMovilesParcial1/Pages/Discografia.xaml.cs
+++ AppMovilesParcial1/Pages/Discografia.xaml.cs
@@ -7,17 +7,30 @@ public partial class Discografia: ContentPage
     private List<Album> _albums =
     [
         new Album {
-            Name="AM",
-            Year="2013",
+            Name="La Vida Era Más Corta",
+            Year="2025",
             Image="lavida.png",
-            Description="ishduiwuid"
+            Description="Su tercer álbum de estudio, donde realiza un giro hacia el folklore y las raíces argentinas, fusionándolo con el trap. Incluye colaboraciones con leyendas como Mercedes Sosa (de manera póstuma), Silvio Rodríguez, Soledad y Trueno"
         },
 
         new Album {
-            Name="Favourite Worst Nightmare",
-            Year="2007",
-            Image="fwn.jpg",
-            Description="uedhwueo"
+            Name="166",
+            Year="2024",
+            Image="cien.png",
+            Description="Su segundo álbum de estudio, enfocado principalmente en el trap. El nombre es un homenaje a la línea de colectivo que usaba para moverse por su barrio."
+        },
+
+         new Album {
+            Name="111",
+            Year="2023",
+            Image="cienonce.png",
+            Description="Su álbum de estudio debut. Incluye colaboraciones con artistas como Peso Pluma, Nicki Nicole, Yami Safdie y Yahritza y Su Esencia. El álbum explora una variedad de sonidos urbanos y mostró su versatilidad al mundo."
+        },
+        new Album {
+            Name="En dormir sin Madrid",
+            Year="2023",
+            Image="madrid.png",
+            Description="Un EP colaborativo con el reconocido productor Bizarrap. Incluye la exitosa \"Milo J: BZRP Music Sessions, Vol. 57\" y otras cuatro canciones que consolidaron su salto a la fama internacional"
         }
     ];
 
