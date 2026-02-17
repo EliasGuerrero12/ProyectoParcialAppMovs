c1956247aa89bcbc732dafe9455d0d5af285197c
Debainilla <debanhiabigailla@gmail.com>
2026-02-17 13:38:39 -0600

    Discografia
    Add Discografia

--- AppMovilesParcial1/Pages/DiscografiaPage.xaml.cs
+++ AppMovilesParcial1/Pages/DiscografiaPage.xaml.cs
@@ -1,22 +1,51 @@
+using AppMovilesParcial1.Models;
+
 namespace AppMovilesParcial1.Pages;
 
 public partial class DiscografiaPage : ContentPage
 {
+    private List<Album> _albums =
+    [
+        new Album {
+            Name="La Vida Era Más Corta",
+            Year="2025",
+            Image="lavida.png",
+            Description="Su tercer álbum de estudio, donde realiza un giro hacia el folklore y las raíces argentinas, fusionándolo con el trap. Incluye colaboraciones con leyendas como Mercedes Sosa (de manera póstuma), Silvio Rodríguez, Soledad y Trueno"
+        },
+
+        new Album {
+            Name="166",
+            Year="2024",
+            Image="cien.png",
+            Description="Su segundo álbum de estudio, enfocado principalmente en el trap. El nombre es un homenaje a la línea de colectivo que usaba para moverse por su barrio."
+        },
+
+        new Album {
+            Name="111",
+            Year="2023",
+            Image="cienonce.png",
+            Description="Su álbum de estudio debut. Incluye colaboraciones con artistas como Peso Pluma, Nicki Nicole, Yami Safdie y Yahritza y Su Esencia. El álbum explora una variedad de sonidos urbanos y mostró su versatilidad al mundo."
+        },
+
+        new Album {
+            Name="En dormir sin Madrid",
+            Year="2023",
+            Image="madrid.png",
+            Description="Un EP colaborativo con el reconocido productor Bizarrap. Incluye la exitosa \"Milo J: BZRP Music Sessions, Vol. 57\" y otras cuatro canciones que consolidaron su salto a la fama internacional"
+        }
+    ];
+
     public DiscografiaPage()
     {
         InitializeComponent();
+        albumsListView.ItemsSource = _albums;
+    }
 
-        List<string> albumes = new List<string>
+    private async void OnAlbumSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem is Album selectedAlbum)
         {
-            "La vida era mas corta (2025)",
-            "166 (2024)",
-            "111 (2023)",
-            "En dormir sin Madrid (2023)",
-            "511 (2023)"
-        };
-
-        ListaAlbumes.ItemsSource = albumes;
+            await Navigation.PushAsync(new AlbumDetail(selectedAlbum));
+        }
     }
 }
-
-
